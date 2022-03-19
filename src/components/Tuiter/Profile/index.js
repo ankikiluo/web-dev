b77141824ad01e19c4dd9e21082e98517a990439
ankikiluo <luo.an@northeastern.edu>
2022-03-19 13:29:32 -0400

    fix json

--- src/components/Tuiter/Profile/index.js
+++ src/components/Tuiter/Profile/index.js
@@ -4,10 +4,14 @@ const Profile = () => {
     const profileData = useSelector(state => state.profileData);
     return (
         <>
-            {profileData.map && profileData.map(profile => {
-                return(<ProfileItem key={profile.handle}
-                                     profile={profile}/>);
-            })}
+
+            <ProfileItem key={profileData.handle}
+                                profile={profileData}/>
+
+            {/*{profileData.map && profileData.map(profile => {*/}
+            {/*    return(<ProfileItem key={profile.handle}*/}
+            {/*                         profile={profile}/>);*/}
+            {/*})}*/}
         </>
     );
 }
