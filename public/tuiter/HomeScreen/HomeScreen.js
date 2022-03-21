bf3d7921d94b41951f1a377203a35918b3ba7321
ankikiluo <luo.an@northeastern.edu>
2022-03-21 18:15:03 -0400

    fix navi bugs

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -8,7 +8,7 @@ import HomeComponent from "./HomeComponent.js";
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar("Home")}
             </div>
-            <div class="col-2 col-md-10 col-lg-7 col-xl-6">
+            <div class="col-10 col-md-10 col-lg-7 col-xl-6">
                 ${HomeComponent()}
             </div>
             <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
--- public/tuiter/NavigationSidebar/NavigationItem.js
+++ public/tuiter/NavigationSidebar/NavigationItem.js
@@ -3,7 +3,7 @@ const NavigationItem = (navi, active) => {
      <a class="list-group-item list-group-item-action ${active===navi.linkName ? 'active':''}" href="${navi.link}">
         <div class="row">
             <div class="col-2"><i class="${navi.fontAwesome}"></i></div>
-            <div class="col-10 d-lg-none d-xl-block"><span>${navi.linkName}</span></div>
+            <div class="col-10 d-none d-xl-inline"><span>${navi.linkName}</span></div>
         </div>
     </a>
     `);
