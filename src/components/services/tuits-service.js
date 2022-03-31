694483594cf91f038d0498c6ea5907d223ec46fc
ankikiluo <luo.an@northeastern.edu>
2022-03-30 22:16:42 -0400

    set api path in tuits-service

--- src/components/services/tuits-service.js
+++ src/components/services/tuits-service.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'https://web-server-node-cs5610.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
-const TUITS_API = 'http://localhost:4000/api/tuits';
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
